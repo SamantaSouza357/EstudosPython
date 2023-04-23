335cc01a1358f900427ad16714ba6addafe0c99f
Samanta Souza <52580206+SamantaSouza357@users.noreply.github.com>
2023-04-22 21:14:40 -0300

    exercicios

---
+++ dobroTiploRaiz.py
@@ -0,0 +1,2 @@
+n = int(input('Digite um Número:' ))
+print(f'O dobro de {n} vale {n * 2} \n O triplo de {n} vale {n * 3} \n A raiz quadrada de {n} vale {n ** (1/2):.2f} ')
---
+++ pintandoParede.py
@@ -0,0 +1,6 @@
+larg = float(input('Largura da Parede:'))
+alt = float(input('Altura da Parede:'))
+area = larg * alt
+print(f'Sua parede tem a dimensão de {larg} x {alt} e sua área é de {area} m²')
+tinta = area / 2
+print(f'Para pintar essa parede, você precisara de {tinta}L de tinta')
